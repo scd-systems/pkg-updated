205e2d2d03b7a1d8b98044c0736300f69c29ee0c
bofh-hannibal <bofh@scd-systems.net>
2016-03-17 22:44:38 +0100

    removed port workdir

--- port/work/.license-catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=BSD3CLAUSE
-_LICENSE_NAME=BSD 3-clause New or Revised License
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF OSI COPYFREE
-_LICENSE_DISTFILES=scd-systems-pkg-updated-0.2.9_GH0.tar.gz
